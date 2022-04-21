aafe37c98bd96c61aabf2ecbfbf29d64d38f0b54
Namrata Baruva <namrata.baruva@cctech.co.in>
2022-04-21 08:04:36 +0530

    gitinit

---
+++ longest_string.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc == 1)
+        printf("");
+    else if (argc == 2)
+        printf(argv[1]);
+    else if (argc > 2)
+    {
+        char *longest = argv[1];
+        for (int i = 1; i < argc; i++)
+        {
+            if (strlen(argv[i]) > strlen(longest))
+            {
+                longest = argv[i];
+            }
+        }
+        printf(longest);
+    }
